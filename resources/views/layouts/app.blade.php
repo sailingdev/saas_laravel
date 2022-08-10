431c3e670f6f277b3a94314f0a2958f95ae0e814
sailingdev <sailingdev123@gmail.com>
2022-08-11 00:21:56 +0900

    0810-3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,8 @@
 @include('layouts.top')
     <!-- Preloader-->
-{{--    <div id="preloader">--}}
-{{--        <div class="wimax-load"></div>--}}
-{{--    </div>--}}
-{{--    commented by yinsong--}}
+    <div id="preloader">
+        <div class="wimax-load"></div>
+    </div>
 @include("layouts.header")
 @yield('content')
 @include("layouts.footer")
